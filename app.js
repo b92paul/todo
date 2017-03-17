324546aefca5dbfba16b8583317686169a2e462a
b92paul <b92paul@gmail.com>
2017-03-17 18:09:17 +0800

    fixed router for '/'

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ var express = require('express');
 var redis = require('redis');
 
 var app = express();
-var router = express.Router();
 var client = redis.createClient();
 
-client.on("connect", function () {
+client.on("connect", () => {
   client.set("foo_rand000000000000", "some fantastic value", redis.print);
   client.get("foo_rand000000000000", redis.print);
 });
 
-app.use(express.static('public'));
-router.get('/', function(req, res){
-  return
+app.use('/static', express.static('public'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(5000);
