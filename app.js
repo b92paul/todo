e1d06acfd6fee9461992570dcea737114628fea0
b92paul <b92paul@gmail.com>
2017-03-20 03:22:50 +0800

    simple login routing with passport (with session)

--- app.js
+++ app.js
@@ -3,41 +3,64 @@ let express = require('express'),
   bodyParser = require('body-parser'),
   passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy;
+	path = require('path');
+
+// set view render engine
+app.set('views', path.join(__dirname, 'src/pug'));
+app.set('view engine', 'pug');
 
 let db = require('./db');
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    db.users.getUser(username, function (err, pass) {
+    db.users.getUser(username, function (err, user) {
       if (err) { return done(err); }
-      if (!pass) {
+      if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
       }
-      if (password !== pass) {
+      if (user.password !== password) {
         return done(null, false, { message: 'Incorrect password.' });
       }
-      return done(null, true);
+			console.log(user);
+      return done(null, user);
     });
   }
 ));
 
+passport.serializeUser(function(user, done) {
+	console.log(user.username);
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  db.users.getUser(username, function (err, user) {
+    done(err, user);
+  });
+});
+
 app.use('/static', express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'asdf234y987', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/public/index.html');
+  res.render('index', {user:req.user});
 });
 
 app.post('/login', passport.authenticate('local', {
 	successRedirect: '/success',
 	failureRedirect: '/',
-	session: false // login without session!
 }));
 
 app.get('/success', (req, res) => {
-  res.sendFile(__dirname+'/public/success.html');
+  console.log(req.user);
+	res.render('success', {user: req.user})
+})
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 })
 
 app.listen(5000);
